c2bed68a9b4f37f9b11b047db4a3df6595e1043c
jhk <2296406413@qq.com>
2019-02-14 20:39:00 +0800

    Initial commit

---
+++ src/main/java/com/admin/shumashop/ShumashopApplication.java
@@ -0,0 +1,14 @@
+package com.admin.shumashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShumashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShumashopApplication.class, args);
+    }
+
+}
+
